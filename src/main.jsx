13ec39251392311f36bf7eb6e700357040632124
yjibus01 <yjibus01@gmail.com>
2025-08-13 17:38:06 +0900

    컴포넌트 분류 및 배경색 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './Tamagotchi.jsx'
-import './Tamagotchi.css'
+import './styles/Header.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
